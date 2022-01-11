25597104083287f1e7dd205ddeb3aaf2b00f3331
Ben <lavaud.benoit@hotmail.com>
2022-01-11 18:41:11 +0100

    Refacto with arrays

---
+++ AsciiArt/FileChecker.cs
@@ -0,0 +1,38 @@
+﻿public class FileChecker
+{
+    public int LinesCount { get; private set; }
+
+    public string[]? ArrayLines { get; set; }
+
+    public FileChecker(string path)
+    {
+        if (File.Exists(path))
+        {
+            if (File.ReadLines(path).Any())
+            {
+                ArrayLines = new string[File.ReadLines(path).Count()];
+
+                foreach (string line in File.ReadLines(path))
+                {
+                    ArrayLines[LinesCount] = line;
+                    LinesCount++;
+                }
+            }
+        }
+        else
+        {
+            throw new ArgumentException("File doesn't exist.");
+        }
+    }
+
+    public void WriteFile()
+    {
+        if (ArrayLines != null)
+        {
+            foreach (string s in ArrayLines)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -1,18 +1,102 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World to the ASCII Game!");
+﻿try
+{
+    FileChecker file2 = new FileChecker(@"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt.txt");
+    FileChecker file = new FileChecker(@"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt2.txt");
+    string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
 
-string path = @"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt.txt";
+    if (file == null ||
+        file.ArrayLines == null ||
+        file.ArrayLines[0].Length <= 0)
+    {
+        throw new Exception("Check your file mf.");
+    }
 
-// Read the ASCII file
-BenTools.ReadFile(path);
+    // width
+    int L = file.ArrayLines[0].Length / 27;
+    Console.WriteLine(L);
+    // Height
+    int H = file.LinesCount;
+    Console.WriteLine(H);
+    // Text line to transform
+    string T = "manhattan";
+    Console.WriteLine(T);
 
-// Set an ASCII Index
-AsciiIndex asciiIndex = new AsciiIndex(path);
+    string[,] IndexLetters = new string[27, L + 1];
+    int count4 = 0;
+    string letterParts = String.Empty;
+    int letterCount = 0;
 
-// Add ASCII bits to definition
-asciiIndex.WriteLetter("A");
-asciiIndex.WriteString("Ben");
-asciiIndex.WriteString("B&n");
+    // for each line
+    for (int i = 0; i < H; i++)
+    {
+        // read each characters
+        foreach (char c in file.ArrayLines[i])
+        {
+            if (letterCount < 27)
+            {
+                // Make a string of letter parts
+                letterParts += c.ToString();
 
+                // Store last part before end of loop
+                if (letterCount == 26 && i == L)
+                {
+                    // Store to Index
+                    IndexLetters[letterCount, i] = letterParts;
+                }
 
-Console.ReadLine();
+                if (count4 == L-1)
+                {
+                    // Store to Index
+                    IndexLetters[letterCount, i] = letterParts;
+                    letterParts = String.Empty;
+                    count4 = 0;
+                    letterCount++;
+                }
+                else
+                {
+                    count4++;
+                }
+            }
+        }
+        letterCount = 0;
+    }
+
+    // Write an answer using Console.WriteLine()
+    // To debug: Console.Error.WriteLine("Debug messages...");
+
+    // Store Letters index
+    int[] charIndex = new int[T.ToCharArray().Count()];
+    int countChar = 0;
+
+    foreach (char c in T.ToCharArray())
+    {
+        int idx = abc.IndexOf(c.ToString().ToUpper());
+        if (idx == -1)
+        {
+            charIndex[countChar] = 26;
+        }
+        else
+        {
+            charIndex[countChar] = idx;
+        }
+        countChar++;
+    }
+
+    // Show T
+    string line = String.Empty;
+    // For each lines
+    for (int i = 0; i < H; i++)
+    {
+        // Foreach characters
+        foreach (int c in charIndex)
+        {
+            line += IndexLetters[c, i];
+        }
+        Console.WriteLine(line);
+        line = String.Empty;
+    }
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine(ex);
+}
