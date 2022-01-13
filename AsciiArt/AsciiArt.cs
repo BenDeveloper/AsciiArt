08f93a7c77d1d133f9b08dfffdd74ff175df5098
Ben <lavaud.benoit@hotmail.com>
2022-01-13 18:26:28 +0100

    Refacto program for unit tests

---
+++ AsciiArt/AsciiArt.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+public class AsciiArt
+{
+	private string alphabet;
+    private FileChecker file;
+    // Characters Index
+    private string[,] IndexChars;
+    // Characters width
+    private int L = 0;
+    // Line count
+    int H = 0;
+
+    /// <summary>
+    /// AsciiArt class constructor.
+    /// </summary>
+    /// <param name="path">Path of the Ascii Art file.</param>
+    /// <param name="abc">String of Characters represented in the file.</param>
+    /// <exception cref="Exception"></exception>
+    public AsciiArt(string path, string abc)
+    {
+        Console.WriteLine("AsciiArt Initialisation");
+        this.alphabet = abc;
+        file = new FileChecker(path);
+
+        if (file == null ||
+            file.ArrayLines == null ||
+            file.ArrayLines[0].Length <= 0)
+        {
+            throw new Exception("Check your file mf.");
+        }
+
+        // Char Width
+        L = file.ArrayLines[0].Length / this.alphabet.Length;
+        Console.WriteLine("Char width : " + L);
+
+        // Char Height
+        H = file.LinesCount;
+        Console.WriteLine("Char height : " + H);
+
+        // Char index two dimentionnal array
+        IndexChars = new string[this.alphabet.Length, L+1];
+        // Width char count
+        int widthCount = 0;
+        // String used to store chars bits
+        string charBits = String.Empty;
+        // Char count
+        int charCount = 0;
+
+        // For each line of characters
+        for (int i = 0; i < H; i++)
+        {
+            // Read each characters
+            foreach (char c in file.ArrayLines[i])
+            {
+                if (charCount <= this.alphabet.Length)
+                {
+                    // Make a string of char bits
+                    charBits += c.ToString();
+
+                    // Store last part before end of loop
+                    if (charCount == this.alphabet.Length -1 && i == L)
+                    {
+                        // Store to Index
+                        // 
+                        IndexChars[charCount, i] = charBits + " ";
+                    }
+
+                    if (widthCount == L-1)
+                    {
+                        // Store to Index
+                        IndexChars[charCount, i] = charBits;
+                        charBits = String.Empty;
+                        widthCount = 0;
+                        charCount++;
+                    }
+                    else
+                    {
+                        widthCount++;
+                    }
+                }
+            }
+            charCount = 0;
+        }
+
+        Write(abc);
+
+        Console.WriteLine("AsciiArt Initialized");
+    }
+
+    public void Write(string s)
+    {
+        // Store Chars index
+        int[] charIndex = new int[s.ToCharArray().Count()];
+        int countChar = 0;
+
+        foreach (char c in s.ToCharArray())
+        {
+            int idx = alphabet.IndexOf(c.ToString().ToUpper());
+            if (idx == -1)
+            {
+                charIndex[countChar] = this.alphabet.Length-1;
+            }
+            else
+            {
+                charIndex[countChar] = idx;
+            }
+            countChar++;
+        }
+
+        // Show T
+        string line = String.Empty;
+        // For each lines
+        for (int i = 0; i < H; i++)
+        {
+            // Foreach characters
+            foreach (int c in charIndex)
+            {
+                line += IndexChars[c, i];
+            }
+            Console.WriteLine(line);
+            line = String.Empty;
+        }
+    }
+}
--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -1,100 +1,14 @@
 ﻿try
 {
-    FileChecker file2 = new FileChecker(@"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt.txt");
-    FileChecker file = new FileChecker(@"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt2.txt");
     string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
+    string path1 = @"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt.txt";
+    string  path2 = @"C:\Users\lavau\source\repos\AsciiArt\AsciiArt\ASCIIArt2.txt";
 
-    if (file == null ||
-        file.ArrayLines == null ||
-        file.ArrayLines[0].Length <= 0)
-    {
-        throw new Exception("Check your file mf.");
-    }
+    AsciiArt asciiArt = new AsciiArt(path1, abc);
 
-    // width
-    int L = file.ArrayLines[0].Length / 27;
-    Console.WriteLine(L);
-    // Height
-    int H = file.LinesCount;
-    Console.WriteLine(H);
-    // Text line to transform
-    string T = "manhattan";
-    Console.WriteLine(T);
+    asciiArt.Write("Hello mf");
 
-    string[,] IndexLetters = new string[27, L + 1];
-    int count4 = 0;
-    string letterParts = String.Empty;
-    int letterCount = 0;
-
-    // for each line
-    for (int i = 0; i < H; i++)
-    {
-        // read each characters
-        foreach (char c in file.ArrayLines[i])
-        {
-            if (letterCount < 27)
-            {
-                // Make a string of letter parts
-                letterParts += c.ToString();
-
-                // Store last part before end of loop
-                if (letterCount == 26 && i == L)
-                {
-                    // Store to Index
-                    IndexLetters[letterCount, i] = letterParts;
-                }
-
-                if (count4 == L-1)
-                {
-                    // Store to Index
-                    IndexLetters[letterCount, i] = letterParts;
-                    letterParts = String.Empty;
-                    count4 = 0;
-                    letterCount++;
-                }
-                else
-                {
-                    count4++;
-                }
-            }
-        }
-        letterCount = 0;
-    }
-
-    // Write an answer using Console.WriteLine()
-    // To debug: Console.Error.WriteLine("Debug messages...");
-
-    // Store Letters index
-    int[] charIndex = new int[T.ToCharArray().Count()];
-    int countChar = 0;
-
-    foreach (char c in T.ToCharArray())
-    {
-        int idx = abc.IndexOf(c.ToString().ToUpper());
-        if (idx == -1)
-        {
-            charIndex[countChar] = 26;
-        }
-        else
-        {
-            charIndex[countChar] = idx;
-        }
-        countChar++;
-    }
-
-    // Show T
-    string line = String.Empty;
-    // For each lines
-    for (int i = 0; i < H; i++)
-    {
-        // Foreach characters
-        foreach (int c in charIndex)
-        {
-            line += IndexLetters[c, i];
-        }
-        Console.WriteLine(line);
-        line = String.Empty;
-    }
+    Console.ReadLine();
 }
 catch (Exception ex)
 {
